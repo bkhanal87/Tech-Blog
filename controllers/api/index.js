0ad6aea47925b981eef98f38e6396d9d822336e4
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-11-03 20:42:34 -0400

    added data to models

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const entriesRoutes = require("./entriesRoutes");
+const commentRoutes = require("./commentRoutes");
+
+router.use("./user", userRoutes);
+router.use("./entries", entriesRoutes);
+router.use('./comment', commentRoutes);
+
+module.exports = router;
